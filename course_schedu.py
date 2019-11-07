42b4e674b66ceabc6c24cd28e0630945f8c94b57
scimmia <affe@live.cn>
2019-11-07 10:10:03 +0800

    finish

--- course_schedu.py
+++ course_schedu.py
@@ -11,6 +11,10 @@ from collections import OrderedDict
 from datas import format_header, format_merge, format_normal
 import utils
 
+sheet_name_class_simple = '排课简略'
+sheet_name_class_all = '排课总览'
+sheet_name_teacher_info = '讲师'
+
 
 class Course(object):
     def __init__(self, c_date, c_time, c_class, teacher):
@@ -89,6 +93,7 @@ def make_mid_file():
     except Exception as e:
         showlog('出错了')
         showlog(str(e))
+    showlog('')
 
 
 def export_mid_file(dates, teacher_classes):
@@ -98,87 +103,87 @@ def export_mid_file(dates, teacher_classes):
     merge_format = work_book.add_format(format_merge)
     normal_format = work_book.add_format(format_normal)
 
-    ws = work_book.add_worksheet('课程')
+    ws = work_book.add_worksheet(sheet_name_class_simple)
     write_dates_to_sheet(ws, dates, merge_format)
     ws.write_row(0, 2, classes.get().split(" "), head_format)
 
-    wsa = work_book.add_worksheet('排课A')
+    wsa = work_book.add_worksheet(sheet_name_class_all + 'A')
     write_dates_to_sheet(wsa, dates, merge_format)
     wsa.write_row(0, 2, teacher_classes.keys(), normal_format)
     wsa.write_row(0, 2 + len(teacher_classes.keys()), classes.get().split(" "), head_format)
 
-    wsb = work_book.add_worksheet('排课')
+    wsb = work_book.add_worksheet(sheet_name_class_all)
     write_dates_to_sheet(wsb, dates, merge_format)
     wsb.write_row(0, 2, teacher_classes.keys(), normal_format)
     wsb.write_row(0, 2 + len(teacher_classes.keys()), classes.get().split(" "), head_format)
 
-    write_teacher_info_sheet(work_book, teacher_classes)
+    write_teacher_info_sheet(work_book, teacher_classes, normal_format)
 
     work_book.close()
     path_course_file.set(file_name)
-    showlog('中间表-已完成，文件为：')
+    showlog('中间表-，文件为：')
     showlog(file_name)
 
 
-def export_class_file(date_list, class_courses, teacher_class):
-    work_book = xlsxwriter.Workbook(path_course_file.get() + datetime.datetime.now().strftime('%H%M') + '排课表.xlsx')
-    head_format = work_book.add_format(format_header)
-    merge_format = work_book.add_format(format_merge)
-    normal_format = work_book.add_format(format_normal)
-
-    for name, class_course in class_courses.items():
-        sheet_name = name
-        ws = work_book.add_worksheet(sheet_name)
-        ws.default_row_height = 20
-        ws.set_column(0, 1, 15)
-        ws.set_column(2, 2, 60)
-        ws.set_column(3, 3, 10)
-        ws.set_column(4, 4, 15)
-        ws.set_column(5, 6, 10)
-        ws.set_row(0, 50)
-        ws.merge_range(0, 0, 0, 6, ('经营网点转型发展业务骨干培训班课程表--%s' % name), head_format)
-        ws.merge_range(1, 0, 1, 4, '', head_format)
-        ws.merge_range(2, 0, 2, 1, '时间', merge_format)
-        ws.write_row(2, 2, ['课程设置', '师资方', '单位', '讲师', '备注'], merge_format)
-        startrow = 3
-        for index, course in enumerate(class_course):
-            ws.write_row(startrow + index, 0, course.get_data(), normal_format)
-
-    time_list = ['8:30-12:00', '14:00-17:30', '19:00-22:30']
-
-    for name, class_course in teacher_class.items():
-        row_datas = {}
-        for d in date_list:
-            row_datas[d] = {}
-            for t in time_list:
-                row_datas[d][t] = ''
-
-        sheet_name = name.replace('/', '、')
-        ws = work_book.add_worksheet(sheet_name)
-        ws.default_row_height = 20
-        ws.set_column(0, 3, 22)
-        ws.set_row(0, 30)
-        ws.merge_range(0, 0, 0, 3, ('%s专家授课表' % name), head_format)
-        ws.write_row(1, 1, ['8:30-12:00', '14:00-17:30', '19:00-22:30'], merge_format)
-        for index, course in enumerate(class_course):
-            try:
-                row_datas[course.c_date][course.c_time] += course.c_class + ' '
-            except:
-                pass
-        startrow = 2
-        for index, d in enumerate(date_list):
-            ws.write(startrow + index, 0, d, merge_format)
-            for n in range(len(time_list)):
-                ws.write(startrow + index, 1 + n, row_datas[d][time_list[n]], normal_format)
-
-    work_book.close()
+# # 生成排课表
+# def export_class_file(date_list, class_courses, teacher_class):
+#     work_book = xlsxwriter.Workbook(path_course_file.get() + datetime.datetime.now().strftime('%H%M') + '排课表.xlsx')
+#     head_format = work_book.add_format(format_header)
+#     merge_format = work_book.add_format(format_merge)
+#     normal_format = work_book.add_format(format_normal)
+#
+#     for name, class_course in class_courses.items():
+#         sheet_name = name
+#         ws = work_book.add_worksheet(sheet_name)
+#         ws.default_row_height = 20
+#         ws.set_column(0, 1, 15)
+#         ws.set_column(2, 2, 60)
+#         ws.set_column(3, 3, 10)
+#         ws.set_column(4, 4, 15)
+#         ws.set_column(5, 6, 10)
+#         ws.set_row(0, 50)
+#         ws.merge_range(0, 0, 0, 6, ('经营网点转型发展业务骨干培训班课程表--%s' % name), head_format)
+#         ws.merge_range(1, 0, 1, 4, '', head_format)
+#         ws.merge_range(2, 0, 2, 1, '时间', merge_format)
+#         ws.write_row(2, 2, ['课程设置', '师资方', '单位', '讲师', '备注'], merge_format)
+#         startrow = 3
+#         for index, course in enumerate(class_course):
+#             ws.write_row(startrow + index, 0, course.get_data(), normal_format)
+#
+#     time_list = ['8:30-12:00', '14:00-17:30', '19:00-22:30']
+#
+#     for name, class_course in teacher_class.items():
+#         row_datas = {}
+#         for d in date_list:
+#             row_datas[d] = {}
+#             for t in time_list:
+#                 row_datas[d][t] = ''
+#
+#         sheet_name = name.replace('/', '、')
+#         ws = work_book.add_worksheet(sheet_name)
+#         ws.default_row_height = 20
+#         ws.set_column(0, 3, 22)
+#         ws.set_row(0, 30)
+#         ws.merge_range(0, 0, 0, 3, ('%s专家授课表' % name), head_format)
+#         ws.write_row(1, 1, ['8:30-12:00', '14:00-17:30', '19:00-22:30'], merge_format)
+#         for index, course in enumerate(class_course):
+#             try:
+#                 row_datas[course.c_date][course.c_time] += course.c_class + ' '
+#             except:
+#                 pass
+#         startrow = 2
+#         for index, d in enumerate(date_list):
+#             ws.write(startrow + index, 0, d, merge_format)
+#             for n in range(len(time_list)):
+#                 ws.write(startrow + index, 1 + n, row_datas[d][time_list[n]], normal_format)
+#
+#     work_book.close()
 
 
 #  在sheet表中填写时间
 def write_dates_to_sheet(ws, dates, format, start_row=1):
     ws.default_row_height = 20
     ws.set_column(0, 1, 15)
-    ws.set_column(2, 6, 10)
     for keys, values in dates.items():
         size = len(values)
         if size > 1:
@@ -190,83 +195,23 @@ def write_dates_to_sheet(ws, dates, format, start_row=1):
             start_row += 1
 
 
-def export_course(teacher_dict, dates, class_dict, teacher_datas):
-    work_book = xlsxwriter.Workbook(
-        path_course_file.get() + '--排课表' + datetime.datetime.now().strftime('%H%M') + '.xlsx')
+# 生成排课表
+def export_course(teacher_dict, dates, class_dict):
+    file_name = path_course_file.get() + '--排课表' + datetime.datetime.now().strftime('%H%M') + '.xlsx'
+    work_book = xlsxwriter.Workbook(file_name)
     head_format = work_book.add_format(format_header)
     merge_format = work_book.add_format(format_merge)
     normal_format = work_book.add_format(format_normal)
 
     write_date_class_sheet(work_book, dates, class_dict)
-    write_teacher_info_sheet(work_book, teacher_dict)
-    for class_name, teachers in class_dict.items():
-        ws = work_book.add_worksheet(class_name)
-        ws.default_row_height = 20
-        ws.set_column(0, 1, 15)
-        ws.set_column(2, 2, 60)
-        ws.set_column(3, 3, 10)
-        ws.set_column(4, 4, 15)
-        ws.set_column(5, 6, 10)
-        ws.set_row(0, 50)
-        ws.merge_range(0, 0, 0, 6, ('经营网点转型发展业务骨干培训班课程表--%s' % class_name), head_format)
-        ws.merge_range(1, 0, 1, 4, '', head_format)
-        ws.merge_range(2, 0, 2, 1, '时间', merge_format)
-        ws.write_row(2, 2, ['课程设置', '师资方', '单位', '讲师', '备注'], merge_format)
-        start_row = 3
-        write_dates_to_sheet(ws, dates, merge_format, 3)
-        for index, teacher_name in enumerate(teachers):
-            teacher = teacher_dict[teacher_name]
-            ws.write_row(start_row + index, 2, teacher.get_class_data(), normal_format)
-            # start_row += 1
+    write_teacher_info_sheet(work_book, teacher_dict, normal_format)
 
-        # for keys, values in dates.items():
-        #     size = len(values)
-        #     if size > 1:
-        #         ws.merge_range(start_row, 0, start_row + size - 1, 0, keys, head_format)
-        #     else:
-        #         ws.write(start_row, 0, keys, head_format)
-        #     for value in values:
-        #         ws.write(start_row, 1, value, merge_format)
-        #         teacher = teacher_dict[teachers[start_row - 3]]
-        #         ws.write_row(start_row, 2, teacher.get_class_data(), normal_format)
-        #         start_row += 1
-
-    #
-    time_list = ['8:30-12:00', '14:00-17:30', '19:00-22:30']
-
-    for name, datas in teacher_datas.items():
-        row_datas = OrderedDict()
-        for d in dates.keys():
-            row_datas[d] = OrderedDict()
-            for t in time_list:
-                row_datas[d][t] = ''
-
-        sheet_name = name.replace('/', '、')
-        ws = work_book.add_worksheet(sheet_name)
-        ws.default_row_height = 20
-        ws.set_column(0, 3, 22)
-        ws.set_row(0, 30)
-        ws.merge_range(0, 0, 0, 3, ('%s专家授课表' % name), head_format)
-        ws.write_row(1, 1, ['8:30-12:00', '14:00-17:30', '19:00-22:30'], merge_format)
-
-        for index, data in enumerate(datas):
-            try:
-                row_datas[data[0]][data[1]] += data[2] + ' '
-            except:
-                pass
-        startrow = 2
-        index = 0
-        for date, value in row_datas.items():
-            ws.write(startrow + index, 0, date, merge_format)
-            ws.write_row(startrow + index, 1, value.values(), normal_format)
-            index += 1
-
-        # for index, d in enumerate(date_list):
-        #     ws.write(startrow + index, 0, d, merge_format)
-        #     for n in range(len(time_list)):
-        #         ws.write(startrow + index, 1 + n, row_datas[d][time_list[n]], normal_format)
+    write_class_info_sheet(work_book, class_dict, teacher_dict, dates, head_format, merge_format, normal_format)
+    write_teacher_time_sheet(work_book, class_dict, dates, head_format, merge_format, normal_format)
 
     work_book.close()
+    showlog('排课表-，文件为：')
+    showlog(file_name)
     pass
 
 
@@ -275,7 +220,7 @@ def get_class_course_from_schedu(book):
     for c in classes.get().split(' '):
         class_courses[c] = []
     if True:
-        sh = book.sheet_by_name('排课')
+        sh = book.sheet_by_name(sheet_name_class_all)
         row_one = sh.row_values(0)
         for rx in range(1, sh.nrows):
             row = sh.row(rx)
@@ -298,7 +243,7 @@ def get_class_course_from_schedu(book):
 
 def get_class_course_from_class(book):
     class_courses = OrderedDict()
-    sh = book.sheet_by_name('课程')
+    sh = book.sheet_by_name(sheet_name_class_simple)
     for ix in range(2, sh.ncols):
         cols = sh.col_values(ix)
         if len(cols) > 1:
@@ -306,13 +251,13 @@ def get_class_course_from_class(book):
     return class_courses
 
 
-# 生成排班表
+# 生成排课简略表
 def write_date_class_sheet(work_book, dates, class_courses):
     head_format = work_book.add_format(format_header)
     merge_format = work_book.add_format(format_merge)
     normal_format = work_book.add_format(format_normal)
 
-    ws = work_book.add_worksheet('排班')
+    ws = work_book.add_worksheet(sheet_name_class_simple)
     ws.default_row_height = 20
     ws.set_column(0, 1, 15)
     ws.set_column(2, 6, 10)
@@ -326,16 +271,78 @@ def write_date_class_sheet(work_book, dates, class_courses):
 
 
 # 生成教师信息表
-def write_teacher_info_sheet(work_book, teacher_classes):
-    ws = work_book.add_worksheet('讲师')
+def write_teacher_info_sheet(work_book, teacher_classes, normal_format):
+    ws = work_book.add_worksheet(sheet_name_teacher_info)
     ws.default_row_height = 20
     ws.set_column(0, 0, 15)
     ws.set_column(1, 1, 60)
     ws.set_column(2, 3, 15)
     start_row = 0
     for value in teacher_classes.values():
-        ws.write_row(start_row, 0, value.get_data())
+        ws.write_row(start_row, 0, value.get_data(), normal_format)
         start_row += 1
+
+
+# 生成各班课程表
+def write_class_info_sheet(work_book, class_dict, teacher_dict, dates, head_format, merge_format, normal_format):
+    for class_name, teachers in class_dict.items():
+        ws = work_book.add_worksheet(class_name)
+        ws.default_row_height = 20
+        ws.set_column(2, 2, 60)
+        ws.set_column(3, 3, 10)
+        ws.set_column(4, 4, 15)
+        ws.set_column(5, 6, 10)
+        ws.set_row(0, 40)
+        ws.merge_range(0, 0, 0, 6, ('经营网点转型发展业务骨干培训班课程表--%s' % class_name), head_format)
+        ws.merge_range(1, 0, 1, 4, '', head_format)
+        ws.merge_range(2, 0, 2, 1, '时间', merge_format)
+        ws.write_row(2, 2, ['课程设置', '师资方', '单位', '讲师', '备注'], merge_format)
+        start_row = 3
+        write_dates_to_sheet(ws, dates, merge_format, start_row)
+        for index, teacher_name in enumerate(teachers):
+            teacher = teacher_dict[teacher_name]
+            ws.write_row(start_row + index, 2, teacher.get_class_data(), normal_format)
+    pass
+
+
+# 生成各专家上课时间表
+def write_teacher_time_sheet(work_book, class_courses, dates, head_format, merge_format, normal_format):
+    teacher_datas = {}
+    for class_name, teachers in class_courses.items():
+        index = 0
+        for date, times in dates.items():
+            for time in times:
+                teacher_name = teachers[index]
+                if not teacher_datas.__contains__(teacher_name):
+                    teacher_datas[teacher_name] = []
+                teacher_datas[teacher_name].append((date, time, class_name))
+                index += 1
+    time_list = ['8:30-12:00', '14:00-17:30', '19:00-22:30']
+    for name, datas in teacher_datas.items():
+        row_datas = OrderedDict()
+        for d in dates.keys():
+            row_datas[d] = OrderedDict()
+            for t in time_list:
+                row_datas[d][t] = ''
+
+        sheet_name = name.replace('/', '、')
+        ws = work_book.add_worksheet(sheet_name)
+        ws.default_row_height = 20
+        ws.set_column(0, 3, 22)
+        ws.set_row(0, 30)
+        ws.merge_range(0, 0, 0, 3, ('%s专家授课表' % name), head_format)
+        ws.write_row(1, 1, ['8:30-12:00', '14:00-17:30', '19:00-22:30'], merge_format)
+
+        for index, data in enumerate(datas):
+            if row_datas.__contains__(data[0]):
+                if row_datas[data[0]].__contains__(data[1]):
+                    row_datas[data[0]][data[1]] += data[2] + ' '
+        start_row = 2
+        index = 0
+        for date, value in row_datas.items():
+            ws.write(start_row + index, 0, date, merge_format)
+            ws.write_row(start_row + index, 1, value.values(), normal_format)
+            index += 1
 
 
 def get_dates(sh):
@@ -357,9 +364,9 @@ def get_dates(sh):
     return dates
 
 
-def get_teacher(book):
+def get_teacher_info(book):
     teacher_course = {}
-    sh = book.sheet_by_name('讲师')
+    sh = book.sheet_by_name(sheet_name_teacher_info)
     for rx in range(0, sh.nrows):
         row = sh.row_values(rx)
         # if len(row) == 4:
@@ -369,47 +376,33 @@ def get_teacher(book):
     return teacher_course
 
 
-def make_course():
+def make_course_file():
     if len(path_course_file.get()) <= 0:
         showinfo('提示', '先选择排课文件')
         return
-    # class_courses = OrderedDict()
 
-    teacher_datas = {}
-    if True:
+    try:
         book = xlrd.open_workbook(path_course_file.get())
-        teacher_course = get_teacher(book)
-        for teacher_name in teacher_course.keys():
-            teacher_datas[teacher_name] = []
+        teacher_course = get_teacher_info(book)
 
         if chVarDis.get():
+            sh = book.sheet_by_name(sheet_name_class_simple)
             class_courses = get_class_course_from_class(book)
         else:
+            sh = book.sheet_by_name(sheet_name_class_all)
             class_courses = get_class_course_from_schedu(book)
-
-        sh = book.sheet_by_name('课程')
         dates = get_dates(sh)
-        row_one = sh.row(0)
-        the_date = 'default'
-        col_date = 0
-        for rx in range(1, sh.nrows):
-            row = sh.row(rx)
-            if row[col_date].ctype == 1:
-                if len(row[col_date].value) > 0:
-                    the_date = row[col_date].value
-            elif row[col_date].ctype == 3:
-                temp = xlrd.xldate.xldate_as_datetime(row[col_date].value, 0)
-                the_date = temp.strftime('%m-%d')
-            for ix in range(2, sh.ncols):
-                teacher_name = row[ix].value
-                teacher_datas[teacher_name].append((the_date, row[1].value, row_one[ix].value))
 
-        export_course(teacher_course, dates, class_courses, teacher_datas)
-        pass
+        export_course(teacher_course, dates, class_courses)
+        showlog('已完成')
+    except Exception as e:
+        showlog('出错了')
+        showlog(str(e))
+    showlog('')
 
 
 def main():
-    logs.grid(row=0, column=0, rowspan=6)
+    logs.grid(row=0, column=0, rowspan=16)
 
     Label(root, text="分班情况\n(空格分隔)", ).grid(row=0, column=1, columnspan=1)
     Entry(root, textvariable=classes).grid(row=0, column=2, columnspan=2, )
@@ -417,12 +410,13 @@ def main():
 
     Button(root, text='选择课程文件', command=lambda: utils.selectFile(pathFile)).grid(row=2, column=1)
     Entry(root, textvariable=pathFile).grid(row=3, column=1)
-    Button(root, text='生成排班', command=make_mid_file).grid(row=4, column=1, )
+    Button(root, text='生成排班中间表', command=make_mid_file).grid(row=4, column=1, )
     Separator(root, orient=VERTICAL).grid(row=2, column=2, rowspan=3, sticky="ns")
 
     Button(root, text='选择排课文件', command=lambda: utils.selectFile(path_course_file)).grid(row=2, column=3)
     Entry(root, textvariable=path_course_file).grid(row=3, column=3)
-    Button(root, text='生成课表', command=make_course).grid(row=4, column=3, columnspan=1)
+    Checkbutton(root, text=("从'%s'生成" % sheet_name_class_simple), variable=chVarDis).grid(row=4, column=3, columnspan=1)
+    Button(root, text='生成课表', command=make_course_file).grid(row=5, column=3, columnspan=1)
     root.mainloop()
 
 
@@ -434,6 +428,5 @@ classes = StringVar()
 classes.set('晨曦 晨光 曙光 朝阳 旭日')
 chVarDis = BooleanVar()
 chVarDis.set(False)
-check1 = Checkbutton(root, text="从排课表生成", variable=chVarDis)
 if __name__ == '__main__':
     main()
